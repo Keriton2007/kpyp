5b790460684869d3cebdeb7a768933d0f0569b63
Karina <you@example.com>
2025-05-02 10:00:36 +0300

    Добавила задания 10 дня практики

---
+++ day9/z1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+interface Ix
+{
+    void IxF0(double w);
+    void IxF1();
+}
+
+interface Iy
+{
+    void F0(double w);
+    void F1();
+}
+
+interface Iz
+{
+    void F0(double w);
+    void F1();
+}
+
+class TestClass : Ix, Iy, Iz
+{
+    public double w;
+
+    public TestClass(double w)
+    {
+        this.w = w;
+    }
+
+    
+    public void IxF0(double w)
+    {
+        Console.WriteLine($"IxF0: {w * w}");
+    }
+
+    public void IxF1()
+    {
+        Console.WriteLine($"IxF1: {w + 6}");
+    }
+
+    
+    public void F0(double w)
+    {
+        Console.WriteLine($"IyF0: {Math.Sqrt(w)}");
+    }
+
+    public void F1()
+    {
+        Console.WriteLine($"IyF1: {w + 2}");
+    }
+
+    
+    void Iz.F0(double w)
+    {
+        Console.WriteLine($"IzF0: {w * w + 5}");
+    }
+
+    void Iz.F1()
+    {
+        Console.WriteLine($"IzF1: {w + 10}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TestClass obj = new TestClass(7);
+
+        Console.WriteLine("\nВызываем методы через объект напрямую:");
+        obj.IxF0(obj.w);
+        obj.IxF1();
+        obj.F0(obj.w);
+        obj.F1();
+
+        Console.WriteLine("\nВызываем методы интерфейса Iz через явное приведение:");
+        ((Iz)obj).F0(obj.w);
+        ((Iz)obj).F1();
+
+        Console.WriteLine("\nВызываем методы через интерфейсную ссылку:");
+        Iy iyRef = obj;
+        iyRef.F0(obj.w);
+        iyRef.F1();
+    }
+}
---
+++ day9/z2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+class Vector : IComparable<Vector>
+{
+    private int[] data;
+    private int lowerBound, upperBound;
+
+    
+    public Vector(int lowerBound, int upperBound)
+    {
+        if (lowerBound > upperBound)
+            throw new ArgumentException("Нижняя граница не может быть больше верхней.");
+
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+        data = new int[upperBound - lowerBound + 1];
+    }
+
+   
+    public int this[int index]
+    {
+        get
+        {
+            if (index < lowerBound || index > upperBound)
+                throw new IndexOutOfRangeException("Выход за границы массива.");
+            return data[index - lowerBound];
+        }
+        set
+        {
+            if (index < lowerBound || index > upperBound)
+                throw new IndexOutOfRangeException("Выход за границы массива.");
+            data[index - lowerBound] = value;
+        }
+    }
+
+    
+    public static Vector operator +(Vector v1, Vector v2)
+    {
+        if (v1.lowerBound != v2.lowerBound || v1.upperBound != v2.upperBound)
+            throw new InvalidOperationException("Границы векторов должны совпадать.");
+
+        Vector result = new Vector(v1.lowerBound, v1.upperBound);
+        for (int i = v1.lowerBound; i <= v1.upperBound; i++)
+            result[i] = v1[i] + v2[i];
+
+        return result;
+    }
+
+   
+    public static Vector operator -(Vector v1, Vector v2)
+    {
+        if (v1.lowerBound != v2.lowerBound || v1.upperBound != v2.upperBound)
+            throw new InvalidOperationException("Границы векторов должны совпадать.");
+
+        Vector result = new Vector(v1.lowerBound, v1.upperBound);
+        for (int i = v1.lowerBound; i <= v1.upperBound; i++)
+            result[i] = v1[i] - v2[i];
+
+        return result;
+    }
+
+   
+    public int CompareTo(Vector other)
+    {
+        int sumThis = 0, sumOther = 0;
+        for (int i = lowerBound; i <= upperBound; i++) sumThis += this[i];
+        for (int i = other.lowerBound; i <= other.upperBound; i++) sumOther += other[i];
+
+        return sumThis.CompareTo(sumOther);
+    }
+
+   
+    public void Print()
+    {
+        Console.Write("[ ");
+        for (int i = lowerBound; i <= upperBound; i++)
+            Console.Write(this[i] + " ");
+        Console.WriteLine("]");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Vector v1 = new Vector(1, 5);
+        Vector v2 = new Vector(1, 5);
+
+        
+        for (int i = 1; i <= 5; i++)
+        {
+            v1[i] = i * 2;
+            v2[i] = i * 3;
+        }
+
+        Console.WriteLine("Вектор 1:");
+        v1.Print();
+
+        Console.WriteLine("Вектор 2:");
+        v2.Print();
+
+       
+        Console.WriteLine("Сложение:");
+        Vector sum = v1 + v2;
+        sum.Print();
+
+        Console.WriteLine("Вычитание:");
+        Vector diff = v1 - v2;
+        diff.Print();
+
+        
+        Console.WriteLine("\nРезультат сравнения: " + v1.CompareTo(v2));
+    }
+}
